04347798cf8d9ca0e49dc96c344708882b286fe5
王金强 <136177121@qq.com>
2020-04-09 11:59:08 +0800

    env

--- hdr/index.js
+++ hdr/index.js
@@ -13,33 +13,31 @@ async function init() {
   // camera.speed = 100
   // camera.attachControl(canvas)
 
-  const hdrTexture = await new Promise(resolve => {
-    const hdr = new BABYLON.HDRCubeTexture(
-      './product.hdr',
-      // 'https://playground.babylonjs.com/textures/parking.hdr',
-      scene,
-      512,
-      null,
-      null,
-      true,
-      null,
-      function() {
-        resolve(hdr)
-      }
-    )
-  })
-  scene.environmentTexture = hdrTexture
+  // const hdrTexture = await new Promise(resolve => {
+  //   const hdr = new BABYLON.HDRCubeTexture(
+  //     'https://playground.babylonjs.com/textures/parking.hdr',
+  //     scene,
+  //     512,
+  //     null,
+  //     null,
+  //     true,
+  //     null,
+  //     function() {
+  //       resolve(hdr)
+  //     }
+  //   )
+  // })
+  // scene.environmentTexture = hdrTexture
 
 
+  scene.environmentTexture = new BABYLON.CubeTexture("https://playground.babylonjs.com/textures/parking.env", scene);
 
   await BABYLON.SceneLoader.ImportMeshAsync(
     '',
-    // './',
-    // 'wns.glb',
-    // 'https://www.babylonjs-playground.com/scenes/',
-    'https://playground.babylonjs.com/scenes/',
-    'BoomBox.glb',
-    // 'seagulf.glb',
+    // 'https://playground.babylonjs.com/scenes/',
+    // 'BoomBox.glb',
+    './',
+    'wns.glb',
     scene
   )
   scene.createDefaultCamera(true, true, true)
