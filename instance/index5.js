7c0f270baadd7bb32e294a925f74e9613db2db7d
王金强 <wangjinqiang@gongchang.com>
2020-10-14 19:30:29 +0800

    test file import

---
+++ instance/index5.js
@@ -0,0 +1,19 @@
+// Create scene
+var canvas = document.getElementById("canvas");
+var engine = new BABYLON.Engine(canvas, true);
+
+async function init() {
+  var scene = new BABYLON.Scene(engine);
+
+  await BABYLON.SceneLoader.ImportMeshAsync("", "./assets/", "ylw.glb", scene);
+  alert("file loaded!");
+  scene.createDefaultCameraOrLight(true, true, true);
+  engine.runRenderLoop(function () {
+    scene.render();
+  });
+
+  window.addEventListener("resize", function () {
+    engine.resize();
+  });
+}
+init();
